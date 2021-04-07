bcd76c130c2f44a05d589e9c99d41fd736480688
Walaa Alomari <walaahmad1987@gmail.com>
2021-04-07 20:59:37 +0300

    edit w

--- server.js
+++ server.js
@@ -33,11 +33,11 @@ const client = new pg.Client(process.env.DATABASE_URL);
 function renderAllBooks(req, res) {
   const sqlQuery = `SELECT * FROM tasks;`;
   client.query(sqlQuery)
-  .then(result => {
+    .then(result => {
       // console.log(result.rows[0]);
-      res.render('pages/index', { oneBook: result.rows })
+      res.render('pages/index', { oneBook: result.rows });
     }
-  )
+    );
 }
 // app.get('/',(req,res)=>res.send('work'))
 
@@ -53,40 +53,40 @@ function seeDetails(req, res) {
     }).catch((error) => {
       errorHandler(`Error in getting Database ${error}`);
     });
-  }
-  app.post('/books', addBook);
-  function addBook(req, res) {
-    
-    const sqlQuery = 'INSERT INTO tasks (author,title,isbn,image_url, description)  VALUES ($1, $2, $3, $4,$5) RETURNING id';
+}
+app.post('/books', addBook);
+function addBook(req, res) {
 
-    const sqlArr = [ req.body.author,req.body.title,req.body.isbn, req.body.image,req.body.description]
-    console.log(sqlArr);
-    client.query(sqlQuery, sqlArr)
-      .then((result) => {
-        
-        res.redirect(`/books/${result.rows[0].id}`)
-        //  res.redirect('/')
-      });
-  }
+  const sqlQuery = 'INSERT INTO tasks (author,title,isbn,image_url, description)  VALUES ($1, $2, $3, $4,$5) RETURNING id';
+
+  const sqlArr = [ req.body.author,req.body.title,req.body.isbn, req.body.image,req.body.description];
+  console.log(sqlArr);
+  client.query(sqlQuery, sqlArr)
+    .then((result) => {
+
+      res.redirect(`/books/${result.rows[0].id}`);
+      //  res.redirect('/')
+    });
+}
 
 function updateBook(req,res){
-    let id = req.params.id;
-    let { author,title, isbn, image_url, description} = req.body;
-    let SQL = `UPDATE tasks SET author=$1,title=$2,isbn=$3,image_url=$4,description=$5 WHERE id =$6;`;
-    let safeValues = [author,title, isbn, image_url, description,id];
-    client.query(SQL, safeValues)
+  let id = req.params.id;
+  let { author,title, isbn, image_url, description} = req.body;
+  let SQL = `UPDATE tasks SET author=$1,title=$2,isbn=$3,image_url=$4,description=$5 WHERE id =$6;`;
+  let safeValues = [author,title, isbn, image_url, description,id];
+  client.query(SQL, safeValues)
     .then(() => {
-        res.redirect(`/books/${id}`);
-    })
+      res.redirect(`/books/${id}`);
+    });
 }
 
 function deleteBook(req,res){
   let id =[req.params.id];
   let SQL = `DELETE FROM tasks WHERE id=$1;`;
   client.query(SQL,id)
-  .then(()=>{
-    res.redirect('/');
-  })
+    .then(()=>{
+      res.redirect('/');
+    });
 }
 function search(req, res) {
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
@@ -95,23 +95,25 @@ function search(req, res) {
   superagent.get(url)
     .then(searchRes => searchRes.body.items.map(element => new Book(element.volumeInfo)))
     .then(results =>
-     
-      { 
-        // console.log(results)
-        res.render('pages/searches/show', { allResault: results })}).catch(error => {
-      res.render('pages/error', { err: error })
+
+    {
+      // console.log(results)
+      res.render('pages/searches/show', { allResault: results });}).catch(error => {
+      res.render('pages/error', { err: error });
 
     });
 }
+
+let booksArr = [];
+
 function Book(data) {
   this.image = data.imageLinks ? data.imageLinks.thumbnail : `https://i.imgur.com/J5LVHEL.jpg`;
-  this.title = data.title?data.title :'No title was found'
+  this.title = data.title?data.title :'No title was found';
   this.author = data.authors?data.authors:'....';
   this.description = data.description?data.description :'No description was found';
   this.isbn = data.industryIdentifiers ? `${data.industryIdentifiers[0].type} ${data.industryIdentifiers[0].identifier}` : 'No ISBN found';
   booksArr.push(this);
   // console.log(this)
 }
-let booksArr = [];
 
 client.connect().then(() => app.listen(PORT, () => console.log(`Listening on port: ${PORT}`)));
